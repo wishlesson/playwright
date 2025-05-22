abf6d2ebef9f5bec6b241f41a4f8713cf640a5ae
Shankar Kumar <shankar.kumar@geccomputersystemsllc.com>
2025-05-23 01:23:53 +0530

    added new config file and changed the test spec

--- tests/testLoginOrangeHRM.spec.ts
+++ tests/testLoginOrangeHRM.spec.ts
@@ -1,5 +1,22 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("Login Test for orangeHRM", async({page}) =>{
-    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php");
-})
+test("has title", async ({ page }) => {
+  await page.goto(
+    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+  );
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/OrangeHRM/);
+});
+
+/* test("get started link", async ({ page }) => {
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+}); */
